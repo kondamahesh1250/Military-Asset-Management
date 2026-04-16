bee808d98a11c1fbfc0f210bd13079808ed650ec
k_mahesh <konda.mahesh1250@gmail.com>
2026-04-16 17:54:44 +0530

    update

--- military-asset-management/src/api.js
+++ military-asset-management/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://military-y73e.onrender.com/api"
+  baseURL: "https://military-gxe3.onrender.com/api"
 });
 
 API.interceptors.request.use(req => {
